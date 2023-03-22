8ca4ed5de9a70554646ac104a50509642ea71ec6
Alan Vieira <>
2023-03-22 00:44:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+#a = 5
+#b = 3
+#soma = a + b
+#if soma == 8:
+ #  print('A soma é 8!')
+#else:
+ #  print('A soma não é 8!')
+
+#num1 = int(input('Insira um número:'))
+#num2 = int(input('Insira o segundo número:'))
+#total = num1 + num2 
+#print(f"O valor da soma é {total} ")
+
+ #exemplo do salário
+
+#salario = int(input("Insira seu salário:"))
+
+#if salario <= 1000:
+  #aumento = salario + (salario*0.2)
+  #print(f"Seu salário sofreu um aumento de 20% totalizando { aumento}")
+#elif salario <= 5000:#Aumento de 10%
+  #aumento = salario + (salario*0.1)
+  #print(f"Seu salário sofreu um aumento de 10% totalizando {aumento}")
+
+  #boletim
+
+#media1 = int(input('Insira a nota1:'))
+#media2 = int(input('Insira a nota2:'))
+#media = (nota1 + nota2) / 2
+#if media >= 8:
+#  print("Aluno Aprovado!:")
+#else media <= 5:
+  #print ("Aluno Reprovado")
+
+#nome = input("Insira um nome:")
+#idade = int(input("Insira idade:"))
+
+#if idade >=18:
+  #print(f"Olá {nome} você é maior com {idade} anos")
+#else:
+    #print(f"Olá{nome} você é menor com {idade} anos")
+
+#Case tabuada
+
+#tabuada = int(input("Qual tabuada você deseja:"))
+#cont = 9
+#soma = 0
+#while cont !=0:
+  #soma = tabuada + cont
+  #print(f"{tabuada} + {cont} = {soma}")
+  #cont = cont -1
+
+#parada = ""
+#while True:
+ # fruta = input("Insira o nome de uma fruta: ")
+ # parada = input("Deseja continuar S/N:").upper()
+  #if parada !="S":
+   # break
+
+while True:
+  nome = input("Insira um nome:")
+  idade = int(input("Insira a idade:"))
+  estado = input("Insira o estado:")
+  cont = input("Deseja continuar S/N: ").upper()
+  if cont == "N":
+   break
+
+
+
+
+
+
+
+
+  
